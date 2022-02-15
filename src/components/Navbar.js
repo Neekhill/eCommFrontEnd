e96b5a27468744db3496213326f00c6759831724
nikhil yadav <iamnikhilyadav27@gmail.com>
2022-02-15 11:50:42 +0530

    added useHistory hook to navigate to home page  on logout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import Badge from "@mui/material/Badge";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import { mobile, tablet } from "../responsive";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { logout } from "../redux/userSlice";
 import { clearCart } from "../redux/cartSlice";
 
@@ -81,10 +81,12 @@ function Navbar() {
   const cartQuantity = useSelector((state) => state.cart.cartQuantity);
   const user = useSelector((state) => state.user.currentUser);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleLogout = () => {
     dispatch(logout());
     dispatch(clearCart());
+    navigate("/");
   };
   return (
     <>
